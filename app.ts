c90a126d0a1360b7adc80e6773e3f99852400cba
Selasie5 <selasisepenu5@gmail.com>
2025-01-23 09:14:21 +0000

    Add comments

--- app.ts
+++ app.ts
@@ -3,17 +3,21 @@ import cors from "cors"
 
 const app = express();
 
+//Cors and JSON middleware
 app.use(express.json());
 app.use(cors())
 
+//Port definition
 const PORT = process.env.PORT || 3000;
 
+//Getting date
 const currentDate = new Date();
 const ISOFormatDate = currentDate.toISOString();
 
+//API endpoint
 app.get("/api/v1", (req:Request, res:Response)=>
 {
-    res.json({email: "selasisepenu5@gmail.com",current_datetime: ISOFormatDate,github_url:"https://github.com/Selasie5/hng-task-1"})
+    res.status(200).json({email: "selasisepenu5@gmail.com",current_datetime: ISOFormatDate,github_url:"https://github.com/Selasie5/hng-task-1"})
 })
 
 app.listen(PORT, ()=>
